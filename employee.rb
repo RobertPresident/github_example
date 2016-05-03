3f7e8dda8a02422a528c52617b575d365c713d27
Robert President <bigmike73184@gmail.com>
2016-05-03 12:32:45 -0500

    initial commit

---
+++ employee.rb
@@ -0,0 +1,9 @@
+employee1 = ["John", "Smith", 70000, true]
+employee1 = ["Jane", "doe", 80000, true]
+
+puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
+puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
+
+employee1 = {"first_name" => "John", "last_name" => "Smith", "salary" => 70000, "active" => true}
+
+employee2 = {"first_name" +> "Jane", "last_name" => "Doe",}
